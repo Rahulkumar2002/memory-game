8896919f5818767aaeaabf63cc98d7c4f40b3c32
Rahul Kumar Jha <rahuljhakumar1600@gmail.com>
2021-11-09 12:48:14 -0800

    completed memory-game first time

---
+++ views/app.js
@@ -0,0 +1,119 @@
+console.log("app.js is connected to the terminal");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    //card options :
+
+    const cardArray = [
+        {
+            name: 'fries',
+            img: '../images/fries.png'
+        },
+        {
+            name: 'fries',
+            img: '../images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: '../images/cheeseburger.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: '../images/cheeseburger.png'
+        },
+        {
+            name: 'pizza',
+            img: '../images/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: '../images/pizza.png'
+        },
+        {
+            name: 'hotdog',
+            img: '../images/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img: '../images/hotdog.png'
+        },
+        {
+            name: 'milkshake',
+            img: '../images/milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img: '../images/milkshake.png'
+        },
+        {
+            name: 'ice-cream',
+            img: '../images/ice-cream.png'
+        },
+        {
+            name: 'ice-cream',
+            img: '../images/ice-cream.png'
+        }
+    ]
+    cardArray.sort(() => 0.5 - Math.random())
+
+    const grid = document.querySelector(".grid");
+    let resultDisplay = document.querySelector('#result');
+    let cardsChosen = [];
+    let cardsChosenId = [];
+    let cardsWon = [];
+
+    //creating board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img');
+            card.setAttribute('src', '../images/blank.png');
+            card.setAttribute('data-id', i);
+            card.addEventListener('click', flipcard);
+            grid.appendChild(card)
+        }
+    }
+
+    //match your card
+    function checkForMatch() {
+        const cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+
+        if (optionOneId == optionTwoId) {
+            cards[optionOneId].setAttribute('src', '../images/blank.png')
+            cards[optionTwoId].setAttribute('src', '../images/blank.png')
+            alert('You have clicked the same image!')
+        }
+        else if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', '../images/white.png')
+            cards[optionTwoId].setAttribute('src', '../images/white.png')
+            cards[optionOneId].removeEventListener('click', flipcard)
+            cards[optionTwoId].removeEventListener('click', flipcard)
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', '../images/blank.png')
+            cards[optionTwoId].setAttribute('src', '../images/blank.png')
+            alert('Sorry, try again')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length / 2) {
+            resultDisplay.textContent = 'Congratulations! You found them all!'
+        }
+    }
+
+    //flip your card 
+    function flipcard() {
+        let cardId = this.getAttribute('data-id');
+        cardsChosen.push(cardArray[cardId].name);
+        cardsChosenId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
+        }
+    }
+    createBoard();
+
+})
